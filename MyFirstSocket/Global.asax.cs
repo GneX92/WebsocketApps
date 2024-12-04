09007c846f39a34ebdd466685d2759f6f4b16fee
julian <julian.bliemel@gmx.de>
2024-12-04 23:33:59 +0100

    Add project files.

---
+++ MyFirstSocket/Global.asax.cs
@@ -0,0 +1,43 @@
+﻿using Fleck;
+using System;
+using System.Collections.Generic;
+
+namespace MyFirstSocket
+{
+    public class Global : System.Web.HttpApplication
+    {
+        protected void Application_Start( object sender , EventArgs e )
+        {
+            List<IWebSocketConnection> allSockets = new List<IWebSocketConnection>();
+
+            WebSocketServer server = new WebSocketServer( "ws://127.0.0.1:8081" );
+
+            server.Start( socket =>
+            {
+                socket.OnOpen = () => allSockets.Add( socket );
+
+                socket.OnClose = () => allSockets.Remove( socket );
+
+                socket.OnMessage = message =>
+                {
+                    string response = null;
+
+                    if ( message == "getdate" )
+                    {
+                        response = DateTime.Now.ToShortDateString();
+                    }
+
+                    if ( message == "gettime" )
+                    {
+                        response = DateTime.Now.ToLongTimeString();
+                    }
+
+                    foreach ( IWebSocketConnection s in allSockets )
+                    {
+                        s.Send( response ?? "invalid Request" );
+                    }
+                };
+            } );
+        }
+    }
+}
---
+++ MyFirstSocket/Scripts/websocket.js
@@ -0,0 +1,43 @@
+﻿"use strict";
+
+const socket = new WebSocket("ws://127.0.0.1:8081");
+let counter = 0;
+const users = [];
+
+
+socket.onopen = function () {
+    if (!localStorage.getItem("id").length) {
+        localStorage.setItem("id", guid())
+    }
+
+    users.push(localStorage.getItem("id"))
+    counter++   
+    document.getElementById('counter').innerHTML = `Besucher: ${users.length}`;
+};
+
+socket.onclose = function () {
+    users.splice(users.indexOf(localStorage.getItem("id")), 1);
+    counter--;
+    document.getElementById('counter').innerHTML = `Besucher: ${users.length}`;
+};
+
+socket.onerror = function (event) {
+    document.getElementById('log').innerHTML = "Fehler: " + JSON.stringify(event);
+};
+
+document.getElementById('message').onblur = function () {
+    const message = document.getElementById('message').value;
+    const li = document.createElement("li");
+    li.innerHTML = message;
+    document.getElementById('log').prepend(li);
+};
+
+function guid() {
+    function s4() {
+        return Math.floor((1 + Math.random()) * 0x10000)
+            .toString(16)
+            .substring(1);
+    }
+    return s4() + s4() + '-' + s4() + '-' + s4() + '-' +
+        s4() + '-' + s4() + s4() + s4();
+}
